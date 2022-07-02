f6555d846a46e40037fcce0620a25f05a5f569a8
DeeChael <DeeChael@outlook.com>
2022-07-02 13:19:49 +0800

    Initialized

---
+++ dwm-api/src/main/java/net/deechael/dwm/api/DynamicWorld.java
@@ -0,0 +1,4 @@
+package net.deechael.dwm.api;
+
+public interface DynamicWorld {
+}
---
+++ dwm-core/src/main/java/net/deechael/dwm/DWMPlugin.java
@@ -0,0 +1,10 @@
+package net.deechael.dwm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DWMPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
